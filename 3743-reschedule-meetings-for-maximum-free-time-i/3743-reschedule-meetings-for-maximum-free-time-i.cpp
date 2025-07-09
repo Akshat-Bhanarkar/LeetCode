98cd69dced8728d0a7fa9c1bfd6d2edf01390904
Akshat-Bhanarkar <150055912+Akshat-Bhanarkar@users.noreply.github.com>
2025-07-09 12:00:34 +0530

    Time: 0 ms (100%), Space: 123.3 MB (74.52%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -4,18 +4,15 @@ public:
         int n = startTime.size();
         vector<int> gap(n + 1);
 
-        // Calculate gaps between events
         for (int i = 1; i < n; i++) {
             gap[i] = startTime[i] - endTime[i - 1];
         }
         gap[0] = startTime[0]; // before first event
         gap[n] = eventTime - endTime[n - 1]; // after last event
 
-        // Sliding window of size k + 1
         int i = 0, j = k;
         int maxa = 0, sum = 0;
 
-        // Initial window sum
         for (int w = i; w <= n && w <= j; w++) {
             sum += gap[w];
         }
