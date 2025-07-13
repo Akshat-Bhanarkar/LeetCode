5b69615df9d501f331d660504cc53d30669743ba
Akshat-Bhanarkar <150055912+Akshat-Bhanarkar@users.noreply.github.com>
2025-07-13 11:14:05 +0530

    Time: 2 ms (71.4%), Space: 10.9 MB (95.65%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,57 +1,33 @@
-// \U0001f609\U0001f609\U0001f609\U0001f609Please upvote if it helps \U0001f609\U0001f609\U0001f609\U0001f609
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        // number of operation greater than length we return an empty string
-        if(num.length() <= k)   
-            return "0";
-        
-        // k is 0 , no need of removing /  preforming any operation
-        if(k == 0)
-            return num;
-        
-        string res = "";// result string
-        stack <char> s; // char stack
-        
-        s.push(num[0]); // pushing first character into stack
-        
-        for(int i = 1; i<num.length(); ++i)
-        {
-            while(k > 0 && !s.empty() && num[i] < s.top())
-            {
-                // if k greater than 0 and our stack is not empty and the upcoming digit,
-                // is less than the current top than we will pop the stack top
-                --k;
-                s.pop();
+        string s = ""; s += num[0];
+        for(int i=1;i<num.length();++i) {
+            // if removal attempts are left (k>0)
+            // and nums[i] is less than last element of s
+            // then remove last element of s and come back to nums[i] again
+            if(k > 0 && s.length() > 0 && num[i] < s[s.length()-1]) {
+                s.pop_back();
+                k--;
+                i--;
+                continue;
             }
-            
-            s.push(num[i]);
-            
-            // popping preceding zeroes
-            if(s.size() == 1 && num[i] == '0')
-                s.pop();
+            else
+            s += num[i];
+        }
+        // if removal attempts are left remove characters at the end
+        // because at this point s contains characters in ascending order
+        // so preferable to remove larger characters at the end
+        while(k > 0 && s.length() > 0) {
+            s.pop_back();
+            k--;
+        }
+
+        while(s[0] == '0') {      // remove trailing zeroes if any
+            s.erase(s.begin());
         }
         
-        while(k && !s.empty())
-        {
-            // for cases like "456" where every num[i] > num.top()
-            --k;
-            s.pop();
-        }
-        
-        while(!s.empty())
-        {
-            res.push_back(s.top()); // pushing stack top to string
-            s.pop(); // pop the top element
-        }
-        
-        reverse(res.begin(),res.end()); // reverse the string 
-        
-        if(res.length() == 0)
-            return "0";
-        
-        return res;
-        
-        
+        if(s.length() == 0) s = "0";   // if final s is empty then indicates 0
+        return s;
     }
 };
