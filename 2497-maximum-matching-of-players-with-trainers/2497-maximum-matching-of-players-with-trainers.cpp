ded232c7b5d229c31960adb820c0481eb6a82b38
Akshat-Bhanarkar <150055912+Akshat-Bhanarkar@users.noreply.github.com>
2025-07-13 09:26:56 +0530

    Time: 32 ms (31.16%), Space: 80.3 MB (24.63%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -5,16 +5,14 @@ public:
         std::sort(trainers.begin(), trainers.end());
 
         int count = 0;
-        int i = 0, j = 0; // pointers for players and trainers
-
+        int i = 0, j = 0; 
         while (i < players.size() && j < trainers.size()) {
             if (players[i] <= trainers[j]) {
-                // Match found
+      
                 count++;
                 i++;
                 j++;
             } else {
-                // Try next trainer
                 j++;
             }
         }
